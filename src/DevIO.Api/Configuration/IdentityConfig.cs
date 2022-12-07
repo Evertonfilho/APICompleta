042fefdea6b9a2ac14db5f9c844d7357cb4acacc
evertonfilho <evertonfilho0513@gmail.com>
2022-12-07 07:59:25 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using DevIO.Api.Data;
+using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DevIO.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        //O identity serve para criar e autentificar os usuarios
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //Criando context do banco do entity
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            //Adicionando o entity
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>() //Poder trabalhar com roles 
+                .AddEntityFrameworkStores<ApplicationDbContext>() //  
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddDefaultTokenProviders();// Serve para gerar tokens para adicionar usuarios
+
+            // JWT
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/DevIO.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        //Separa os bancos de dados, esse no caso é armazenado dados de autenticação
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
